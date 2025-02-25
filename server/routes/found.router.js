b464245b92e8e0ecf4ec43992d00f67359fcd102
Alecia Hagen <alecia.h98@gmail.com>
2025-02-25 16:43:08 -0600

    all of the get routes are good to go. Checking on the put, post and delete routes

--- server/routes/found.router.js
+++ server/routes/found.router.js
@@ -1,22 +1,42 @@
 const express = require('express');
 const pool = require('../modules/pool');
 //const axios = 
-
+const rejectUnauthenticated = require('../modules/authentication-middleware');
 const router = express.Router();
 
 
 //FORM POST
-router.post('/', (req, res) => {
-    let sqlText = `INSERT INTO "found" ("item_id","found_date", "location", "description", "user_id", ) VALUES ($1, $2, $3, $4, $5);`
-    pool.query(sqlText, [req.body.item_id, req.body.found_date, req.body.location, req.body.description, req.body.photo, req.user.id])
-    .then((results) => {
-        res.sendStatus(200);
-    }).catch((err) => {
-        console.log(err);
+//NEEDS WORK!!!!!
+// router.post('/', (req, res) => {
+//     let sqlText = `INSERT INTO "found" ("item_id","found_date", "location", "description", "user_id", ) VALUES ($1, $2, $3, $4, $5);`
+//     pool.query(sqlText, [req.body.item_id, req.body.found_date, req.body.location, req.body.description, req.body.photo, req.user.id])
+//     .then((results) => {
+//         res.sendStatus(200);
+//     }).catch((err) => {
+//         console.log(err);
+//         res.sendStatus(500);
+//     });
+// });
+
+
+//GET FOUND LIST
+//MAKE SURE THIS RETURNS A SPECIFIC PERSON'S LIST AND NOT ALL OF THE LISTS
+router.get('/', rejectUnauthenticated, (req, res) => {
+    const query = `
+    SELECT * FROM "found";
+    `;
+    pool.query(query,[req.user.id, req.query])
+     .then(result => {
+        res.send(result.rows);
+     })
+     .catch(err => {
+        console.log(`Error grabbing favorites`, err);
         res.sendStatus(500);
-    });
+     })
 });
 
+
+//DELETE POST FROM FOUND LIST
 
 
 module.exports = router;
--- server/routes/items.router.js
+++ server/routes/items.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 //const axios = 
+const rejectUnauthenticated = require('../modules/authentication-middleware');
 
 const router = express.Router();
 
@@ -20,10 +21,11 @@ router.get('/', (req, res) => {
     })
 });
 
-//
+
+//grabbing by specific category
 router.get('/:category', (req,res) => {
     const query = `
-    SELECT "name", "description", "is_favorite", "found", "season", "uses", "photo", "nutrition", "shelf_life", "harvesting", "imposters", "category"
+    SELECT "id", "name", "description", "is_favorite", "found", "season", "uses", "photo", "nutrition", "shelf_life", "harvesting", "imposters", "category"
     FROM "item"
     WHERE "category" = $1;
     `;
@@ -35,10 +37,53 @@ router.get('/:category', (req,res) => {
             console.log('ERROR GRABBING CATEGORY', err);
             res.sendStatus(500);
         })
-})
+});
 
-//getting a specific item description
-router.get('/')
+
+//getting a specific item from the specified category list
+router.get('/:itemId', (req, res) => {
+    const query = `
+    SELECT "id", "name", "description", "is_favorite", "found", "season", "uses", "photo", "nutrition", "shelf_life", "harvesting", "imposters", "category"
+    FROM "item"
+    WHERE "id" = $1;
+    `;
+    pool.query(query,[req.params.itemId])
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('Error:', err);
+      res.sendStatus(500);
+    })
+});
+
+
+//GRABBING THE FAVORITES LIST
+//MAKE SURE IT IS PULLING THE LOG IN USER'S FAVORITE ITEMS
+router.get('/favorites', rejectUnauthenticated, (req, res) => {
+    const query = `
+    SELECT "id", "name", "is_favorite"
+    FROM "item"
+    WHERE "is_favorite" = TRUE ;
+    `;
+    pool.query(query,[req.user.id])
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('Error:', err);
+      res.sendStatus(500);
+    })  
+});
+
+//PUT ROUTE TO SWITCH AN ITEM'S FAVORITE KEY
+//MAKE SURE TO INCLUDE THE REQ.USER WHEN WRITING THIS
+// router.put('/favorites/:id', (req, res) => {
+//     const query = `
+//     UPDATE
+//     `
+
+// })
 
 
 module.exports = router;
