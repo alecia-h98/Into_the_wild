461d8afba37a179e39009df47bc666a4ec5c3691
Alecia Hagen <alecia.h98@gmail.com>
2025-03-02 21:41:32 -0600

    FINALLY figured out the put. Updated it on the backend, zustand store and within the page itself

--- server/routes/favorites.router.js
+++ server/routes/favorites.router.js
@@ -26,7 +26,7 @@ router.get('/', (req, res) => {
     })
 });
 
-//DONE? - WORKS ON THE BACKEND
+//DONE
 //POST FUNCTION TO MAKE A FAVORITE
 router.post('/:itemId', (req, res) => {
     const query = `
@@ -47,22 +47,27 @@ router.post('/:itemId', (req, res) => {
 });
 
 
-
-//DONE
+//Done??
 // PUT ROUTE TO SWITCH AN ITEM'S FAVORITE KEY
 // MAKE SURE TO INCLUDE THE REQ.USER WHEN WRITING THIS
-router.put('/fav/:favId', (req, res) => {
+router.put('/fav', (req, res) => {
+    //Below is making a request to grab the id from the req.body
+    const { id } = req.body;
+    
+    if(!id){
+        return res.status(400).json({ error: "Missing favorite ID"});
+    }
     const sqlText = `
-    UPDATE "user_item" 
-SET "is_favorited" = FALSE 
-WHERE "is_favorited" = TRUE 
-AND "user_item"."id" = $1;
+        UPDATE "user_item" 
+        SET "is_favorited" = FALSE 
+        WHERE "is_favorited" = TRUE 
+        AND "user_item"."id" = $1;
     `;
-    pool.query(sqlText, [req.params.favId]).then((result) => {
-        res.send(result.rows);
-    }).catch(err => {
-        res.sendStatus(500);
-        console.error(err);
+    pool.query(sqlText, [id])
+        .then(() => res.sendStatus(200))
+        .catch(err => {
+            console.error(err);
+            res.sendStatus(500);
     })
 });
 
--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -5,6 +5,9 @@ import useStore from "../../zustand/store";
 function Favorites() {
   const favorites = useStore((state) => state.favorites);
   const switchFav = useStore((state) => state.switchFav);
+  // const favorite = useStore((state) => state.favorite);
+  const removeFavorite = useStore((state) => state.removeFavorite);
+  // const fetchFavorites = useStore((state) => state.fetchFavorites);
   // const navigate = useNavigate();
   console.log(favorites);
 
@@ -15,14 +18,14 @@ function Favorites() {
   //   navigate(`/items/${category}`)
   // }
 
-  const unfavorite = (event) => {
-    const favId = event.target.id;
-    console.log(favId);
-    // switchFav(favId);
-  }
+  
 
-
-  //onClick={handleClick}
+  // const unfavorite = (event) => {
+  //   // const favId = event.target.id;
+  //   // console.log(favId);
+  //   switchFav(event);
+  //   // fetchFavorites();
+  // }
 
   return (
     <div>
@@ -33,12 +36,11 @@ function Favorites() {
             <div key={item.id} id={item.id}>
               <h3 id={item.id}> {item.name} </h3>
               <img  src={item.photo} alt={item.name} />
-              <button onClick={unfavorite} >Unfavorite</button>
+              <button onClick={() => removeFavorite(item.id)}>Unfavorite</button>
               <br />
             </div>
           );
         })}
-        
       </section>
     </div>
   )
