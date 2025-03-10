8c5aefc1174686ac3986dd837e0efa02fdc7760b
Alecia Hagen <alecia.h98@gmail.com>
2025-03-10 02:23:15 -0500

    Updated the category selection page and the icons on the main menu

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -31,12 +31,13 @@ console.log(categories, "This is the all categories page");
     <>
     <div id="background" >
       <h1 id='mcHead' >Pick a category:</h1>
-      <section className='buttons'>
+      <p>By picking a category it will direct you to foragable items</p>
+      <section className='cat'>
         {categories?.map((category) => {
           return (
-            <div key={category.id} id={category.id}>
-              <img src={category.photo} />
-              <Button variant="dark" id={category.id} onClick={handleClick}>
+            <div className="grid grid-cols-2 items-center gap-4" key={category.id} id={category.id}>
+              {/* <img className="w-16 h-16 object-cover" src={category.photo} /> */}
+              <Button className="text-lg font-semibold" variant="dark" id={category.id} onClick={handleClick}>
                 {category.id ===1
                 ? "Other"
                 :category.id ===2
@@ -47,6 +48,7 @@ console.log(categories, "This is the all categories page");
                 ? "Herbs"
                 : "Nuts & Seeds"}
                 </Button>
+                <br />
             </div>
           )
         })}
